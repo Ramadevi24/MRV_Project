aa1f2feb4fbde73f6b9cf317cd7da792d2530503
Ramadevi24 <ramadevi.taraka@ispatialtec.com>
2024-09-05 22:36:41 +0530

    View Organization modal

--- src/components/organization/CompanyProfileGrid.jsx
+++ src/components/organization/CompanyProfileGrid.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
-import { useCompanyProfile } from '../../contexts/CompanyProfileContext';
-import { toast } from 'react-toastify';
-import ViewCompanyProfileModal from './ViewCompanyProfileModal'
-import formatDate from '../../utils/formateDate';
-import axios from 'axios';
-import '../../css/TableGrid.css';
-import eyeicon from '../../images/eyeicon.png';
-import editicon from '../../images/editicon.png';
-import deleteicon from '../../images/deleteicon.png';
+import React, { useState } from "react";
+import { useCompanyProfile } from "../../contexts/CompanyProfileContext";
+import { toast } from "react-toastify";
+import ViewCompanyProfileModal from "./ViewCompanyProfileModal";
+import axios from "axios";
+import "../../css/TableGrid.css";
+import eyeicon from "../../images/eyeicon.png";
+import editicon from "../../images/editicon.png";
+import deleteicon from "../../images/deleteicon.png";
 
-const CompanyProfileGrid = ({onEdit}) => {
+const CompanyProfileGrid = ({ onEdit }) => {
   const { companyProfiles, deleteCompanyProfile, fetchCompanyProfiles } = useCompanyProfile();
   const [showViewModal, setShowViewModal] = useState(false);
   const [selectedProfile, setSelectedProfile] = useState(null);
@@ -21,13 +19,24 @@ const CompanyProfileGrid = ({onEdit}) => {
       await fetchCompanyProfiles();
       toast.success(`Organization deleted successfully.`);
     } catch (error) {
-      toast.error(`Error deleting company profile with ID ${id}: ${error.message}`, error.stack);
+      toast.error(
+        `Error deleting company profile with ID ${id}: ${error.message}`,
+        error.stack
+      );
     }
   };
 
-  const handleView = (profile) => {
-    setSelectedProfile(profile);
+  const handleView = async (profile) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/Organization/${profile.organizationID}`
+      );
+      const organizationData = response.data;
+      setSelectedProfile(organizationData); 
     setShowViewModal(true);
+    } catch (error) {
+      console.error("Error fetching organization data:", error);
+    }
   };
 
   const handleCloseViewModal = () => {
@@ -37,16 +46,17 @@ const CompanyProfileGrid = ({onEdit}) => {
 
   const handleEdit = async (profile) => {
     try {
-      const response = await axios.get(`http://localhost:5000/api/Organization/${profile.organizationID}`);
+      const response = await axios.get(
+        `http://localhost:5000/api/Organization/${profile.organizationID}`
+      );
       const organizationData = response.data;
       onEdit(organizationData); // Pass the fetched data to the parent component
     } catch (error) {
-      console.error('Error fetching organization data:', error);
+      console.error("Error fetching organization data:", error);
     }
   };
   return (
     <>
-    
       {/* <Table striped bordered hover style={{width:'95%', marginLeft:'35px'}}>
         <thead>
           <tr>
@@ -72,33 +82,58 @@ const CompanyProfileGrid = ({onEdit}) => {
         </tbody>
       </Table> */}
       <table className="custom-table">
-<thead className='tabel-head'>
-<tr>
-<th><input className='check-box' type="checkbox" /></th>
+        <thead className="tabel-head">
+          <tr>
+            <th>
+              <input className="check-box" type="checkbox" />
+            </th>
             <th>Tenant Name</th>
             <th>Organization Name</th>
+            <th>Categories</th>
             <th>Actions</th>
-</tr>
-</thead>
-<tbody>
-{companyProfiles.map((profile) => (
- <tr key={profile.organizationID}>
-  <td><input className='check-box' type="checkbox" /></td>
-<td>{profile.tenantName}</td>
-              <td>{profile.organizationName}</td>
-              
+          </tr>
+        </thead>
+        <tbody>
+          {companyProfiles.map((profile) => (
+            <tr key={profile.organizationID}>
               <td>
-<span className="action-icons">
-<button className="view-btn"  onClick={() => handleView(profile)}><img src={eyeicon} /></button>
-<button onClick={() => handleEdit(profile)} className="edit-btn"><img src={editicon} /></button>
-<button   onClick={() => handleDelete(profile.organizationID)}className="delete-btn"><img src={deleteicon} /></button>
-</span>
-</td>
-</tr>
-        ))}
-</tbody>
-</table>
-      <ViewCompanyProfileModal show={showViewModal} handleClose={handleCloseViewModal} companyProfile={selectedProfile} />
+                <input className="check-box" type="checkbox" />
+              </td>
+              <td>{profile.tenantName}</td>
+              <td>{profile.organizationName}</td>
+              <td>{profile.categories.$values.join(',')}</td>
+
+              <td>
+                <span className="action-icons">
+                  <button
+                    className="view-btn"
+                    onClick={() => handleView(profile)}
+                  >
+                    <img src={eyeicon} />
+                  </button>
+                  <button
+                    onClick={() => handleEdit(profile)}
+                    className="edit-btn"
+                  >
+                    <img src={editicon} />
+                  </button>
+                  <button
+                    onClick={() => handleDelete(profile.organizationID)}
+                    className="delete-btn"
+                  >
+                    <img src={deleteicon} />
+                  </button>
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <ViewCompanyProfileModal
+        show={showViewModal}
+        handleClose={handleCloseViewModal}
+        companyProfile={selectedProfile}
+      />
     </>
   );
 };
