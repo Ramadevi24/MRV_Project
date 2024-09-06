98f0cb0a4798bca33e23b60d80cfb02495068b0a
Ramadevi24 <ramadevi.taraka@ispatialtec.com>
2024-09-06 17:03:17 +0530

    Datamanagement Integration

--- src/pages/DataManagement.js
+++ src/pages/DataManagement.js
@@ -1,33 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Table, Dropdown, DropdownButton, Modal, Button, Form } from "react-bootstrap";
-import '../css/DataManagement.css';
+import "../css/DataManagement.css";
+import DataManagementAPI from "../services/DataManagementAPI";
 
 const DataManagement = () => {
   const [selectedFuelType, setSelectedFuelType] = useState("All Fuels");
-  const [fuelData, setFuelData] = useState([
-    {
-      fuelName: "Coal",
-      primaryFuels: "Solid",
-      netCalorificValue: "24 MJ/kg",
-      fuelType: "Solid Fuels",
-    },
-    {
-      fuelName: "Petrol",
-      primaryFuels: "Liquid",
-      netCalorificValue: "44 MJ/kg",
-      fuelType: "Liquid Fuels",
-    },
-    // Add more data as needed
-  ]);
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [editFormData, setEditFormData] = useState({
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [currentFuelId, setCurrentFuelId] = useState(null);
+  const [formData, setFormData] = useState({
+    fuelType: "Solid",
     fuelName: "",
-    primaryFuels: "",
+    isPrimaryFuel: "Yes",
     netCalorificValue: "",
-    fuelType: "",
+    carbonContentNCV: "",
+    grossCalorificValue: "",
+    carbonContentGCV: "",
+    conversionFactorType: "NCV",
   });
   const [showUserDefined, setShowUserDefined] = useState(false);
   const [conversionFactorType, setConversionFactorType] = useState("NCV");
+  const [fuelData, setFuelData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await DataManagementAPI.getAll();
+        setFuelData(data);
+      } catch (error) {
+        console.error("Error fetching fuel data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleCheckboxChange = () => {
     setShowUserDefined(!showUserDefined);
@@ -41,31 +46,85 @@ const DataManagement = () => {
     setSelectedFuelType(fuelType);
   };
 
-  const handleEditClick = (fuel) => {
-    setEditFormData(fuel);
-    setShowEditModal(true);
+  const handleDeleteClick = async (fuelId) => {
+    try {
+      await DataManagementAPI.delete(fuelId);
+      setFuelData(fuelData.filter((fuel) => fuel.fuelID !== fuelId));
+    } catch (error) {
+      console.error("Error deleting fuel:", error);
+    }
   };
 
-  const handleDeleteClick = (fuelName) => {
-    setFuelData(fuelData.filter((fuel) => fuel.fuelName !== fuelName));
-  };
-
-  const handleEditFormChange = (e) => {
+  const handleFormChange = (e) => {
     const { name, value } = e.target;
-    setEditFormData((prevData) => ({
+    setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
 
-  const handleEditFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    setFuelData((prevData) =>
-      prevData.map((fuel) =>
-        fuel.fuelName === editFormData.fuelName ? editFormData : fuel
-      )
-    );
-    setShowEditModal(false);
+    const updatedFormData = {
+      ...formData,
+      isPrimaryFuel: formData.isPrimaryFuel === "Yes" ? true : false,
+    };
+    try {
+      if (isEditMode) {
+        const editUpdatedFormData = {
+            ...updatedFormData,
+            fuelID: currentFuelId,
+        }
+        await DataManagementAPI.update(currentFuelId, editUpdatedFormData);
+        setFuelData((prevFuelData) =>
+          prevFuelData.map((fuel) =>
+            fuel.fuelID === currentFuelId ? { ...fuel, ...editUpdatedFormData } : fuel
+          )
+        );
+      } else {
+        // Create new fuel
+        const newFuel = await DataManagementAPI.create(updatedFormData);
+        setFuelData([...fuelData, newFuel]);
+      }
+      handleCloseModal();
+    } catch (error) {
+      console.error("Error managing fuel:", error);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowAddModal(false);
+    setIsEditMode(false);
+    setFormData({
+      fuelType: "Solid",
+      fuelName: "",
+      isPrimaryFuel: "Yes",
+      netCalorificValue: "",
+      carbonContentNCV: "",
+      grossCalorificValue: "",
+      carbonContentGCV: "",
+      conversionFactorType: "NCV",
+    });
+  };
+
+  const handleEditClick = (fuel) => {
+    handleShowEditModal(fuel);
+  };
+
+  const handleShowEditModal = (fuel) => {
+    setIsEditMode(true);
+    setCurrentFuelId(fuel.fuelID);
+    setFormData({
+      fuelType: fuel.fuelType,
+      fuelName: fuel.fuelName,
+      isPrimaryFuel: fuel.isPrimaryFuel ? "Yes" : "No",
+      netCalorificValue: fuel.netCalorificValue,
+      carbonContentNCV: fuel.carbonContentNCV,
+      grossCalorificValue: fuel.grossCalorificValue,
+      carbonContentGCV: fuel.carbonContentGCV,
+      conversionFactorType: fuel.conversionFactorType,
+    });
+    setShowAddModal(true);
   };
 
   const filteredFuelData =
@@ -75,118 +134,198 @@ const DataManagement = () => {
 
   return (
     <div style={{ padding: "20px" }}>
-        <div className="table-header">
+      <div className="table-header">
         <h2>Fuel Manager</h2>
         <div className="filters">
-      <div className="button-box">
-        <input
-          type="checkbox"
-          id="userDefined"
-          checked={showUserDefined}
-          onChange={handleCheckboxChange}
-        />
-        <label htmlFor="userDefined">Show user-defined fuels only</label>
+          <div className="button-box">
+            <input
+              type="checkbox"
+              id="userDefined"
+              checked={showUserDefined}
+              onChange={handleCheckboxChange}
+            />
+            <label htmlFor="userDefined">Show user-defined fuels only</label>
+          </div>
+          <div className="button-box toggle-button">
+            <label>Conversion Factor Type:</label>
+            <button
+              className={conversionFactorType === "NCV" ? "active" : ""}
+              onClick={() => handleToggleClick("NCV")}
+            >
+              NCV
+            </button>
+            <button
+              className={conversionFactorType === "GCV" ? "active" : ""}
+              onClick={() => handleToggleClick("GCV")}
+            >
+              GCV
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="button-box toggle-button">
-        <label>Conversion Factor Type:</label>
-        <button
-          className={conversionFactorType === "NCV" ? "active" : ""}
-          onClick={() => handleToggleClick("NCV")}
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={selectedFuelType}
+          onSelect={handleFuelTypeChange}
         >
-          NCV
-        </button>
-        <button
-          className={conversionFactorType === "GCV" ? "active" : ""}
-          onClick={() => handleToggleClick("GCV")}
-        >
-          GCV
-        </button>
+          <Dropdown.Item eventKey="All Fuels">All Fuels</Dropdown.Item>
+          <Dropdown.Item eventKey="Liquid">Liquid Fuels</Dropdown.Item>
+          <Dropdown.Item eventKey="Solid">Solid Fuels</Dropdown.Item>
+        </DropdownButton>
+        <Button onClick={() => setShowAddModal(true)}>Add New Fuel</Button>
       </div>
-    </div>
-      </div>
-      <DropdownButton
-        id="dropdown-basic-button"
-        title={selectedFuelType}
-        onSelect={handleFuelTypeChange}
-      >
-        <Dropdown.Item eventKey="All Fuels">All Fuels</Dropdown.Item>
-        <Dropdown.Item eventKey="Liquid Fuels">Liquid Fuels</Dropdown.Item>
-        <Dropdown.Item eventKey="Solid Fuels">Solid Fuels</Dropdown.Item>
-      </DropdownButton>
       <Table striped bordered hover className="mt-3">
         <thead>
           <tr>
             <th>Fuel Name</th>
-            <th>Primary Fuels</th>
-            <th>Net Calorific Value</th>
+            <th>Primary Fuel</th>
+            <th>Net Calorific Value (TJ/Gg)</th>
+            <th>Carbon Content (NCV) (Kg C/GJ)</th>
             <th>Fuel Type</th>
-            <th>Action</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {filteredFuelData.map((fuel, index) => (
-            <tr key={index}>
+          {filteredFuelData?.map((fuel) => (
+            <tr key={fuel.fuelID}>
               <td>{fuel.fuelName}</td>
-              <td>{fuel.primaryFuels}</td>
+              <td>{fuel.isPrimaryFuel ? "Yes" : "No"}</td>
               <td>{fuel.netCalorificValue}</td>
+              <td>
+                {fuel.carbonContentNCV} || {fuel.carbonContentGCV}
+              </td>
               <td>{fuel.fuelType}</td>
               <td>
                 <button onClick={() => handleEditClick(fuel)}>Edit</button>
-                <button onClick={() => handleDeleteClick(fuel.fuelName)}>Delete</button>
+                <button onClick={() => handleDeleteClick(fuel.fuelID)}>
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
 
-      <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
+      <Modal show={showAddModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Edit Fuel</Modal.Title>
+          <Modal.Title>{isEditMode ? "Edit Fuel" : "Add New Fuel"}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={handleEditFormSubmit}>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group controlId="formFuelType">
+              <Form.Label>Fuel Type</Form.Label>
+              <div className="d-flex">
+                <Form.Check
+                  type="radio"
+                  label="Solid"
+                  name="fuelType"
+                  value="Solid"
+                  checked={formData.fuelType === "Solid"}
+                  onChange={handleFormChange}
+                  style={{ marginRight: "40px" }}
+                />
+                <Form.Check
+                  type="radio"
+                  label="Liquid"
+                  name="fuelType"
+                  value="Liquid"
+                  checked={formData.fuelType === "Liquid"}
+                  onChange={handleFormChange}
+                />
+              </div>
+            </Form.Group>
             <Form.Group controlId="formFuelName">
               <Form.Label>Fuel Name</Form.Label>
               <Form.Control
                 type="text"
                 name="fuelName"
-                value={editFormData.fuelName}
-                onChange={handleEditFormChange}
-                readOnly
+                value={formData.fuelName}
+                onChange={handleFormChange}
               />
             </Form.Group>
-            <Form.Group controlId="formPrimaryFuels">
-              <Form.Label>Primary Fuels</Form.Label>
-              <Form.Control
-                type="text"
-                name="primaryFuels"
-                value={editFormData.primaryFuels}
-                onChange={handleEditFormChange}
-              />
+            <Form.Group controlId="formIsPrimaryFuel">
+              <Form.Label>Is Primary Fuel</Form.Label>
+              <div className="d-flex">
+                <Form.Check
+                  type="radio"
+                  label="Yes"
+                  name="isPrimaryFuel"
+                  value="Yes"
+                  checked={formData.isPrimaryFuel === "Yes"}
+                  onChange={handleFormChange}
+                  style={{ marginRight: "40px" }}
+                />
+                <Form.Check
+                  type="radio"
+                  label="No"
+                  name="isPrimaryFuel"
+                  value="No"
+                  checked={formData.isPrimaryFuel === "No"}
+                  onChange={handleFormChange}
+                />
+              </div>
             </Form.Group>
             <Form.Group controlId="formNetCalorificValue">
               <Form.Label>Net Calorific Value</Form.Label>
               <Form.Control
-                type="text"
+                type="number"
                 name="netCalorificValue"
-                value={editFormData.netCalorificValue}
-                onChange={handleEditFormChange}
+                value={formData.netCalorificValue}
+                onChange={handleFormChange}
               />
             </Form.Group>
-            <Form.Group controlId="formFuelType">
-              <Form.Label>Fuel Type</Form.Label>
+            <Form.Group controlId="formCarbonContentNCV">
+              <Form.Label>Carbon Content NCV</Form.Label>
               <Form.Control
-                as="select"
-                name="fuelType"
-                value={editFormData.fuelType}
-                onChange={handleEditFormChange}
-              >
-                <option>Liquid Fuels</option>
-                <option>Solid Fuels</option>
-              </Form.Control>
+                type="number"
+                name="carbonContentNCV"
+                value={formData.carbonContentNCV}
+                onChange={handleFormChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formGrossCalorificValue">
+              <Form.Label>Gross Calorific Value</Form.Label>
+              <Form.Control
+                type="number"
+                name="grossCalorificValue"
+                value={formData.grossCalorificValue}
+                onChange={handleFormChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formCarbonContentGCV">
+              <Form.Label>Carbon Content GCV</Form.Label>
+              <Form.Control
+                type="number"
+                name="carbonContentGCV"
+                value={formData.carbonContentGCV}
+                onChange={handleFormChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formConversionFactorType">
+              <Form.Label>Conversion Factor Type</Form.Label>
+              <div className="d-flex">
+                <Form.Check
+                  type="radio"
+                  label="NCV"
+                  name="conversionFactorType"
+                  value="NCV"
+                  checked={formData.conversionFactorType === "NCV"}
+                  onChange={handleFormChange}
+                  style={{ marginRight: "40px" }}
+                />
+                <Form.Check
+                  type="radio"
+                  label="GCV"
+                  name="conversionFactorType"
+                  value="GCV"
+                  checked={formData.conversionFactorType === "GCV"}
+                  onChange={handleFormChange}
+                />
+              </div>
             </Form.Group>
             <Button variant="primary" type="submit">
-              Save Changes
+              {isEditMode ? "Update" : "Submit"}
             </Button>
           </Form>
         </Modal.Body>
